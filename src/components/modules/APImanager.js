1c1eed0e9a02da29f446f9930abf13a8581e9cb3
MelanieJaneBond <miss.bond2@gmail.com>
2019-07-22 13:42:43 -0500

    input file structure

---
+++ src/components/modules/APImanager.js
@@ -0,0 +1,38 @@
+const remoteURL = "http://localhost:5002"
+
+export default {
+    get (resource, id) {
+            return fetch(`${remoteURL}/${resource}/${id}`).then(e => e.json())
+
+    },
+    all (resource) {
+            return fetch(`${remoteURL}/${resource}`).then(e => e.json())
+        },
+
+    post (resource, resourceObj) {
+            return fetch(`${remoteURL}/${resource}`, {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json"
+                },
+                body: JSON.stringify(resourceObj)
+              }).then(data => data.json())
+
+        },
+
+    delete(resource, id) {
+            return fetch(`${remoteURL}/${resource}/${id}`, {
+                method: "DELETE"
+                })
+        },
+
+    put(resource, editedObj) {
+            return fetch(`${remoteURL}/${resource}/${editedObj.id}`, {
+              method: "PUT",
+              headers: {
+                "Content-Type": "application/json"
+              },
+              body: JSON.stringify(editedObj)
+            }).then(data => data.json());
+          }
+}
