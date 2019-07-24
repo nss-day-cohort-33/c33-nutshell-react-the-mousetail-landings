75a319f40a727c4cc01e48160a023b142b205df4
MelanieJaneBond <miss.bond2@gmail.com>
2019-07-24 10:13:17 -0500

    moved EventList data into EventForm to simplify rendering on the home page

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,7 +1,6 @@
 import { Route, Redirect } from "react-router-dom";
 import React, { Component } from "react";
 import { withRouter } from "react-router";
-import NavBar from "./nav/NavBar";
 
 // import ArticleManager from "../modules/ArticleManager"
 import EventManager from "./modules/EventManager"
@@ -9,14 +8,10 @@ import EventManager from "./modules/EventManager"
 import TaskManager from "./modules/TaskManager"
 import LoginManager from "./modules/LoginManager"
 import TaskList from "./task/TaskList"
-<<<<<<< HEAD
-import EventList from "./event/EventList"
+// import EventList from "./event/EventList"
 import EventForm from "./event/EventForm"
-=======
-import ArticleList from "./article/ArticleList"
-import EventList from "./event/EventList"
-import MessageList from "./message/MessageList"
->>>>>>> c85dbe919ff17101c275bb4b5168a604e54f8db1
+// import ArticleList from "./article/ArticleList"
+// import MessageList from "./message/MessageList"
 import Login from "./authentication/Login"
 import Welcome from "./authentication/Welcome"
 import Register from "./authentication/Register"
@@ -65,10 +60,7 @@ addTask = (task) => {
 
 getUser = (userName) => {
   return LoginManager.get("user", userName)
-
 }
-
-
 
   render() {
     return (
@@ -77,42 +69,30 @@ getUser = (userName) => {
         <Route exact path="/" component={Welcome}/>
 
         <Route exact path="/home" render={props => {
-            return ( <TaskList  {...props} tasks={this.state.tasks} deleteTask={this.deleteTask} />)
-              // (<ArticleList  {...props} articles={this.state.articles} deleteArticle={this.deleteArticle} />)
-              // (<EventList  {...props} events={this.state.events} deleteEvent={this.deleteEvent} />)
-              // (<MessageList  {...props} messages={this.state.messages} deleteMessage={this.deleteMessage} />)
-          }}/>
+          return (<EventForm  {...props} events={this.state.events} deleteEvent={this.deleteEvent} addEvent={this.addEvent} />)
+            // <TaskList  {...props} tasks={this.state.tasks} deleteTask={this.deleteTask} />
+              // <ArticleList  {...props} articles={this.state.articles} deleteArticle={this.deleteArticle} />
+              // <MessageList  {...props} messages={this.state.messages} deleteMessage={this.deleteMessage} />
+          }} />
 
         <Route
-<<<<<<< HEAD
-          path="/events" render={props => {
-            if(this.isAuthenticated()) {
-            return <EventList {...props} deleteEvent={this.deleteEvent} events={this.state.events} />
-          } else {
-          return <Redirect to="/login" /> }
-=======
           exact path="/register" render={props => {
             return ( <Register {...props} users={this.state.users} addNewUser={this.addNewUser}/>);
->>>>>>> c85dbe919ff17101c275bb4b5168a604e54f8db1
           }}
           />
-          <Route path="/events/new" render={(props) => {
-              return <EventForm
-                {...props}
-                addEvent={this.addEvent} />
-            }} />
-
-
-
-        <Route exact path="/login" component={Login} />
 
         <Route
-          path="/events" render={props => {
-            return null
-            // Remove null and return the component which will show the user's events
-          }}
-        />
+          path="/events/all" render={props => {
+          }} />
+            
+        <Route path="/events" render={(props) => {
+          if(this.isAuthenticated()) {
+            return <EventForm {...props} addEvent={this.addEvent} deleteEvent={this.deleteEvent} events={this.state.events} />
+          } else {
+            return <Redirect to="/login" /> 
+          }}} />
 
+        <Route exact path="/login" component={Login} />
 
         <Route
           path="/friends" render={props => {
--- src/components/event/EventForm.js
+++ src/components/event/EventForm.js
@@ -65,6 +65,26 @@ export default class EventForm extends Component {
                 </div>
                 <button onClick={this.handleSubmit}>Save</button>
             </form>
+            <section className="events">
+            {
+                this.props.events.map(each =>
+                    <div key={each.userId} className="card">
+                        <div className="card-body">
+                            <div className="card-title">
+                                {/* <img src={dog} className="icon--dog" /> */}
+                                <h2>{each.title}</h2>
+                                <h3>{each.location}</h3>
+                                <h4>{each.date}</h4>
+                                <h4>{each.time}</h4>
+                                {/* <button
+                                    onClick={() => this.props.deleteAnimal(animal.id)}
+                                    className="card-link">Delete</button> */}
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+            </section>
         </React.Fragment>
     )
 }}
